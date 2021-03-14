d47421c9d586bf459dbced08753437714890f758
CherkashinD <cherkashin.du@gmail.com>
2021-03-14 17:16:30 +0200

    add project

---
+++ Generic/Generic/GenericArray.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+
+namespace Generic
+{
+    public class GenericArray<T>
+    {
+        private T[] _array = Array.Empty<T>();
+
+        public GenericArray(params T[] items)
+        {
+            AddRange(items);
+        }
+
+        public int Count => _array.Length;
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+
+                return _array[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+
+                _array[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref _array, Count + 1);
+            _array[Count - 1] = item;
+        }
+
+        public void AddRange(T[] items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public void AddRange(GenericArray<T> items)
+        {
+            foreach (T item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            RemoveAt(Array.IndexOf(_array, item));
+        }
+
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+            var newArray = new T[_array.Length - 1];
+
+            var i = 0;
+            var j = 0;
+            while (i < _array.Length)
+            {
+                if (i != index)
+                {
+                    newArray[j] = _array[i];
+                    j++;
+                }
+
+                i++;
+            }
+
+            _array = newArray;
+        }
+
+        public void Sort()
+        {
+            Array.Sort(_array);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _array[i];
+            }
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Generic
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intArray = new GenericArray<int>();
+
+            intArray.Add(5);
+            intArray.Add(1);
+            intArray.Add(5);
+            intArray.Add(13);
+            intArray.Add(4);
+            intArray.Add(10);
+            intArray.Sort();
+            intArray[1] += 3;
+            ConsoleLog(intArray, "Get sorted array");
+
+            intArray.RemoveAt(1);
+            ConsoleLog(intArray, "Remove item by index");
+
+            intArray.Remove(10);
+            intArray.Remove(13);
+            ConsoleLog(intArray, "Remove item by value");
+
+            intArray.AddRange(new[] { 5, 10, 6, 7, 68, 89, 365 });
+            ConsoleLog(intArray, "Add range of array");
+
+            var array = new GenericArray<int>(165, 15, 212, 488);
+            intArray.AddRange(array);
+            intArray.Sort();
+            ConsoleLog(intArray, "Add range of collection");
+
+            Console.ReadLine();
+        }
+
+        public static void ConsoleLog<T>(GenericArray<T> array, string title)
+        {
+            Console.WriteLine(title);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
